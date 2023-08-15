368f6d9542ee9cd8a99805b6509abb4dbd89d243
ChestnutMongrel <chestnutmongrel@yandex.ru>
2023-08-16 00:05:29 +0300

    Add function to guess word, implement stage 2

--- hangman.py
+++ hangman.py
@@ -1,15 +1,19 @@
+def guess_word() -> None:
+    word = 'python'
+    user_guess = input('Guess the word: ')
+
+    print('You survived!' if word == user_guess else 'You lost!')
+
+
 def print_title() -> None:
     print('H A N G M A N')
 
 
-def print_teaser() -> None:
-    print('The game will be available soon.')
-
-
 def main() -> None:
     print_title()
-    print_teaser()
+    guess_word()
 
 
 if __name__ == '__main__':
     main()
+    
