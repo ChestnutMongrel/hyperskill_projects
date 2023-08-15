075d69a8cb576a9d7b84c785a0424f4c114c0d48
ChestnutMongrel <chestnutmongrel@yandex.ru>
2023-08-15 20:55:39 +0300

    Print messages to screen, implement stage 1

---
+++ hangman.py
@@ -0,0 +1,15 @@
+def print_title() -> None:
+    print('H A N G M A N')
+
+
+def print_teaser() -> None:
+    print('The game will be available soon.')
+
+
+def main() -> None:
+    print_title()
+    print_teaser()
+
+
+if __name__ == '__main__':
+    main()
