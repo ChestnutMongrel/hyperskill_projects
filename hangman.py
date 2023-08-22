3562fee290dab73fee0b2041e291123d6d21f32a
ChestnutMongrel <chestnutmongrel@yandex.ru>
2023-08-22 20:08:14 +0300

    Add an ability to guess the word by letters
    The user has eight attempts now, implement stage 5.

--- hangman.py
+++ hangman.py
@@ -1,4 +1,9 @@
+from collections.abc import Iterable
 from random import choice
+
+
+def get_letter() -> str:
+    return input('Input a letter: ')
 
 
 def get_word() -> str:
@@ -14,30 +19,68 @@ def guess_word() -> None:
     print('You survived!' if word == user_guess else 'You lost!')
 
 
-def make_hint(word: str) -> str:
-    hint_size = 3
-    hint_symbol = '-'
-    len_word = len(word)
+def make_hint(word: str, hint: str, symbol: str) -> str:
+    if len(word) != len(hint):
+        raise ValueError
 
-    if len_word <= hint_size:
-        return hint_symbol * len_word
+    hint = list(hint)
+    for i in symbol_indexes(word, symbol):
+        hint[i] = symbol
 
-    return word[:hint_size] + hint_symbol * (len_word - hint_size)
+    return ''.join(hint)
+
+
+def symbol_indexes(word: str, symbol: str) -> Iterable:
+    # result = list()
+    for i, letter in enumerate(word):
+        if letter == symbol:
+            yield i
+    # return result
+
+
+def test_symbol_indexes() -> None:
+    result = [*symbol_indexes('afaafa', 'a')]
+    assert result == [0, 2, 3, 5], result
+    result = [*symbol_indexes('afafa', 'r')]
+    assert result == [], result
+    result = [*symbol_indexes('', 's')]
+    assert result == [], result
+    result = [*symbol_indexes('asdf', '')]
+    assert result == [], result
 
 
 def test_make_hint() -> None:
-    assert make_hint('python') == 'pyt---'
-    assert make_hint('sdf') == '---'
-    assert make_hint('') == ''
+    result = make_hint('affaaf', '-' * 6, 'a')
+    assert result == 'a--aa-', result
+    result = make_hint('afafaff', '-' * 7, 'v')
+    assert result == '-' * 7, result
 
 
 def print_title() -> None:
     print('H A N G M A N')
+    print()
 
 
 def main() -> None:
     print_title()
-    guess_word()
+
+    word = get_word()
+    hint = '-' * len(word)
+    attempts = 8
+
+    while attempts:
+        print(hint)
+        letter = get_letter()
+
+        if letter not in word:
+            print("That letter doesn't appear in the word.")
+        else:
+            hint = make_hint(word, hint, letter)
+
+        print()
+        attempts -= 1
+
+    print('Thanks for playing!')
 
 
 if __name__ == '__main__':
